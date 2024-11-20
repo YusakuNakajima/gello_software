238c096726880a84203f601b1f2bf4626c3617da
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-11-20 14:00:28 +0900

    Update

--- gello_ros/src/gello_ros/agents/gello_agent.py
+++ gello_ros/src/gello_ros/agents/gello_agent.py
@@ -71,7 +71,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
             4 * np.pi / 2,
             2 * np.pi / 2,
             0 * np.pi / 2,
-            3 * np.pi / 2,
+            3 * np.pi / 2 + np.pi / 4,
             3 * np.pi / 2,
         ),
         joint_signs=(1, 1, -1, 1, -1, 1),
@@ -152,7 +152,6 @@ class GelloAgent(Agent):
             ).astype(int)
             self._robot.command_joint_torque(dynamixel_current_goals)
 
-        dynamixel_joints[4] += np.pi / 4  # for DENSO robot
         return dynamixel_joints
         # current_q = dynamixel_joints[:-1]  # last one dim is the gripper
         current_gripper = dynamixel_joints[-1]  # last one dim is the gripper
