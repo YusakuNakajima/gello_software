0e65c34cf7c18ecd919bd0b01143e4abae311010
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-11-30 17:18:05 +0900

    Update to use noncompressed image

--- gello_ros/src/gello_ros/cameras/realsense_ros.py
+++ gello_ros/src/gello_ros/cameras/realsense_ros.py
@@ -3,7 +3,7 @@ import time
 from typing import List, Optional, Tuple
 import numpy as np
 import rospy
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image, CompressedImage
 import cv2
 
 
@@ -28,14 +28,14 @@ class RealSenseROS:
         flip: bool = False,
     ):
         self._flip = flip
+        height = rospy.get_param("~camera_height", 480)
+        width = rospy.get_param("~camera_width", 640)
 
-        self._empty_color_image = np.zeros((480, 640, 3), dtype=np.uint8)
-        self._empty_depth_image = np.zeros((480, 640), dtype=np.uint16)
-        self._color_image = np.zeros((480, 640, 3), dtype=np.uint8)
-        self._depth_image = np.zeros((480, 640), dtype=np.uint16)
-        self._color_update = False
+        self._empty_color_image = np.zeros((height, width, 3), dtype=np.uint8)
+        self._empty_depth_image = np.zeros((height, width), dtype=np.uint16)
+        self._color_image = None
+        self._depth_image = None
 
-        self._color_img_msg = None
         if prefix is None:
             prefix = ""
         else:
@@ -43,8 +43,8 @@ class RealSenseROS:
 
         # ROS Subscribers
         rospy.Subscriber(
-            "/" + prefix + "camera/color/image_raw/compressed",
-            CompressedImage,
+            "/" + prefix + "camera/color/image_raw",
+            Image,
             self._color_callback,
             queue_size=1,
         )
@@ -56,10 +56,14 @@ class RealSenseROS:
         # )
         print("Subscribers initialized.")
 
-    def _color_callback(self, msg: CompressedImage):
+    def _color_callback(self, msg: Image):
         """Callback for the color image."""
-        self._color_img_msg = msg
-        self._color_update = True
+        try:
+            self._color_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            if self._flip:
+                self._color_image = cv2.rotate(self._color_image, cv2.ROTATE_180)
+        except Exception as e:
+            rospy.logerr(f"Failed to convert image: {e}")
 
     def _depth_callback(self, msg: CompressedImage):
         """Callback for the depth image."""
@@ -74,62 +78,9 @@ class RealSenseROS:
         img_size: Optional[Tuple[int, int]] = None,
     ) -> Tuple[Optional[np.ndarray], Optional[np.ndarray]]:
         """Get the latest color and depth images."""
-        if self._color_img_msg is None:
+        if self._color_image is None:
             return self._empty_color_image, self._empty_depth_image
-        if not self._color_update:
-            return self._color_image, self._empty_depth_image
-        self._color_update = False
-
-        np_arr = np.frombuffer(self._color_img_msg.data, np.uint8)
-        color_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-        # if img_size is None:
-        #     image = color_image[:, :, ::-1]
-        # else:
-        #     image = cv2.resize(color_image, img_size)[:, :, ::-1]
-
-        # rotate 180 degree's because everything is upside down in order to center the camera
-        if self._flip:
-            color_image = cv2.rotate(color_image, cv2.ROTATE_180)
-            # depth = cv2.rotate(depth, cv2.ROTATE_180)[:, :, None]
-        # else:
-        # depth = depth[:, :, None]
-        self._color_image = color_image
-
-        return color_image, self._empty_depth_image
-
-
-def _debug_read(camera, save_datastream=False):
-    import cv2
-
-    cv2.namedWindow("image")
-    cv2.namedWindow("depth")
-    counter = 0
-    if not os.path.exists("images"):
-        os.makedirs("images")
-    if save_datastream and not os.path.exists("stream"):
-        os.makedirs("stream")
-    while not rospy.is_shutdown():
-        time.sleep(0.1)
-        image, depth = camera.read()
-        if image is None or depth is None:
-            continue
-        depth_vis = cv2.applyColorMap(
-            cv2.convertScaleAbs(depth, alpha=0.03), cv2.COLORMAP_JET
-        )
-
-        key = cv2.waitKey(1)
-        cv2.imshow("image", image)
-        cv2.imshow("depth", depth_vis)
-        if key == ord("s"):
-            cv2.imwrite(f"images/image_{counter}.png", image)
-            cv2.imwrite(f"images/depth_{counter}.png", depth_vis)
-        if save_datastream:
-            cv2.imwrite(f"stream/image_{counter}.png", image)
-            cv2.imwrite(f"stream/depth_{counter}.png", depth_vis)
-        counter += 1
-        if key == 27:  # ESC to exit
-            break
+        return self._color_image, self._empty_depth_image
 
 
 if __name__ == "__main__":
